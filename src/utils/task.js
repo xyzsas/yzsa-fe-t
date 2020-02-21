17080e3919914434ce7427f85f44879173b78718
CmdBlock-zqg <zhuqinggangemail@qq.com>
2020-02-22 01:51:16 +0800

    feat: form

--- src/utils/task.js
+++ src/utils/task.js
@@ -2,7 +2,8 @@ export default {
   // 任务类型string: 任务类型描述
   type: {
     'Notice': '通知',
-    'CourseSelect': '选课'
+    'CourseSelect': '选课',
+    'Form': '问卷'
   },
   // 任务信息示例
   infoExample: {
@@ -14,6 +15,23 @@ export default {
       'lesson1': 50,
       'lesson2': 60,
       'and so on': 70
+    },
+    'Form': {
+      '1': {
+        'type': 'choose',
+        'question': '选择题问题描述',
+        'choice': {
+          'A': 'apple',
+          'B': 'banana'
+        },
+        'min': 1,
+        'max': 2
+      },
+      '2': {
+        'type': 'fill',
+        'question': '填空题问题描述',
+        'max': 1000
+      }
     }
   }
 }
